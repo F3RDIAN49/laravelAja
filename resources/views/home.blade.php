8687ed1e331d06c8af18c694cc4f8bbca6a3862c
root <root@CaptainVolkyDesktop.localdomain>
2021-07-25 22:09:15 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+@extends('layout/main')
+<head>
+    @section('title', 'home')
+
+</head>
+<body>
+@section('container')
+<div class="container mt-4">
+    <h1>This is the beginning</h1>
+    <h4>{{ $name }}</h4>
+    <p>email me {{ $email }}</p>
+    <img src="img/{{ $image }}" alt="{{ $name }}" height="200" width="200">
+</div>
+
+<script src="js/script.js"></script>
+</body>
+@endsection
---
+++ resources/views/test.blade.php
@@ -0,0 +1,17 @@
+@extends('layout/main')
+
+<head>
+    @section('title', 'test')
+</head>
+
+<body>
+    @section('container')
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>Test aja inimah da hehe</h1>
+            </div>
+        </div>
+    </div>
+    @endsection
+</body>
